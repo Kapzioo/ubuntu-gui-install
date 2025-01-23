a461f82658c67225b8b4bcd1a0d149777476d51a
Kapzioo <71943086+Kapzioo@users.noreply.github.com>
2025-01-23 20:10:26 +0100

    Delete uninstall-gui.sh

--- uninstall-gui.sh
+++
@@ -1,6 +0,0 @@
-apt install tasksel -y
-tasksel --list-tasks
-tasksel --task-packages desktop
-apt purge task-desktop hyphen-en-us libglu1-mesa libreoffice-* libu2f-udev mythes-en-us x11-apps x11-session-utils xinit xorg xserver-* desktop-base task-german task-german-desktop totem gedit gedit-common gir1.2-* gnome-* gstreamer* sound-icons speech-dispatcher totem-common xserver-* xfonts-* xwayland gir1.2* gnome-* -y
-apt autoremove --purge 
-reboot
